86865351c3c612668d6ff42028c9e4c125caa14f
GNUAaryn <90221167+GNUAaryn@users.noreply.github.com>
2022-12-04 07:55:00 -0600

    Update themes.sh

--- themes.sh
+++ themes.sh
@@ -7,6 +7,9 @@ cp -r ~/arch-setup/wallpapers/ ~/.wallpapers
 echo "Finally, we will be making zsh run as the default shell."
 chsh -s $(which zsh)
 
+# beep boop
+mv /home/aaron/arch-setup/.zshrc /home/aaron
+
 # Small talk
 sleep 2s
 echo "customization has been configured!"
