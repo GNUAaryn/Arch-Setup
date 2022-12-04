05d79be1d8382384c79e7d974bcea2f0507e1193
GNUAaryn <90221167+GNUAaryn@users.noreply.github.com>
2022-12-04 02:42:54 -0600

    Update themes.sh

--- themes.sh
+++ themes.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-# Installs wallpaper themes.
-cp -r ~/Arch-setup/wallpapers/ ~/.wallpapers
-
 # Changes the shell for fish
-echo "Finally, we will be making fish run as the default shell."
+echo "Finally, we will be making zsh run as the default shell."
 chsh -s $(which zsh)
 
 # Small talk
